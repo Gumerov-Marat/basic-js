9d7b1f7c4fa3cc1572068054ed3365c44b899dae
Gumerov-Marat <59646933+Gumerov-Marat@users.noreply.github.com>
2020-02-25 12:49:43 +0300

    feat: add code in count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,21 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  if (arr.length == 0){
+    return 0;
+  }
+  
+  let flattened = arr.reduce(function (a, b) {
+    return a.concat(b);
+  });
+  
+  let quantity = 0;
+  if (quantity.length == 0) {
+    return 0;
+  }
+
+  for (element of flattened) {
+    if (element == "^^") {
+      quantity++;
+    }
+  }
+  return quantity;
 };
