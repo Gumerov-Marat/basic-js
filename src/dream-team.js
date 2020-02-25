57ff3e85aed02187220613147e60ee58fefbdfd0
Gumerov-Marat <59646933+Gumerov-Marat@users.noreply.github.com>
2020-02-25 12:54:25 +0300

    feat: add code in dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    // проверка массива, обработка
+    if (Array.isArray(members) && (!members.length == 0)) {
+      let arr = [];
+      for (element of members) {
+         if (typeof element == "string") {
+          arr.push(element.trim().substr(0, 1).toLocaleUpperCase());
+        }
+      }
+      // сортируем массив по возрастающей
+      function compareNumbers(a, b) {
+        return a - b;
+      }
+      arr.sort();
+      // свертываем буквы в слово
+      let word = arr.join('');
+      return word;
+    }
+    return false;
+  };
